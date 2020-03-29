1f4c0ceb5c8310901fb38eb7a2d788fbc7fa7c64
Kenneth John Locey <ken@weecology.org>
2020-03-29 08:06:13 -0500

    revised SEIR-SD model -KL

--- notebooks/SEIR-SD_testing.py
+++ notebooks/SEIR-SD_testing.py
@@ -26,7 +26,7 @@ def obs_pred_rsquare(obs, pred):
     return 1 - sum((obs - pred) ** 2) / sum((obs - np.mean(obs)) ** 2)
 
 
-def seir_sd(obs_x, obs_y, ForecastDays, init_vals, params, N, t):
+def seir_sd(obs_x, obs_y, ForecastDays, init_vals, params, N, t, sd):
     # This model is derived from: https://towardsdatascience.com/social-distancing-to-slow-the-coronavirus-768292f04296
     
     obs_x = np.array(obs_x)
@@ -46,10 +46,9 @@ def seir_sd(obs_x, obs_y, ForecastDays, init_vals, params, N, t):
     for j in t[1:]:
         
         I_N = I[-1]/N
-        sd = 0.0000000007
-        sd = sd/(sd + I_N)
-        beta = beta * sd
-        #print(beta)
+        
+        sd1 = sd/(sd + I_N)
+        beta = beta * sd1
         
         next_S = ((S[-1] - (beta*S[-1]*I[-1])))
         next_E = (E[-1] + (beta*S[-1]*I[-1] - alpha * E[-1]))
@@ -95,6 +94,7 @@ df = df[df['Country/Region'] == 'US']
 df = df[df['Province/State'] != 'US']
 
 focal_loc = 'Illinois'
+#focal_loc = 'New York'
 df_sub = df[df['Province/State'] == focal_loc]
 df_sub = df_sub.loc[:, (df_sub != 0).any(axis=0)]
 
@@ -122,16 +122,19 @@ N = 12740000
 #S, E, I, R
 init_vals = 1 - 1/N, 1/N, 0, 0
 
-incubation_period = 5 
+incubation_period = 5
 alpha = 1/incubation_period 
 
-infectious_period = 7 
+infectious_period = 7
 gamma = 1/infectious_period 
 
-rho = 4.
+rho = 4
 beta = gamma*rho
 
-        
+
+sd =  0.0000000007
+#sd = 0.00000001
+
 params = alpha, beta, gamma, rho
 
 
@@ -139,7 +142,7 @@ t = list(range(t_max))
 
 
 forecasted_y, forecasted_x, pred_y = seir_sd(obs_x, obs_y, ForecastDays,
-                                        init_vals, params, N, t)
+                                        init_vals, params, N, t, sd)
 
 obs_pred_r2 = obs_pred_rsquare(obs_y, pred_y)  
 print('r2:', obs_pred_r2) 
